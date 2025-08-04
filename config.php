523a913c2f6c62d1a83d14190f86e80fedea3ecf
Kjell Heinsen <ich@kjell-heinsen.de>
2025-08-04 23:25:09 +0200

    Configuration File

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+// Trage hier die URL ein, auf der du dein CDN mit Hilfe des Asset Proxys eingerichtet hast.
+define('CDNURL','https://yourcdn.de/');
+// Füge hier die URL ein, auf der dein eigenes Projekt läuft.
+define('DIR','https://yourwebsiteurl.de/');
